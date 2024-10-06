c381e716054bea1cd73768bc99ea97f8b2cfba77
rain44556 <bristy44556@gmail.com>
2024-10-06 21:54:09 +0600

    added 4 categories button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 // click viewMore to go main section
 const adoptInfo = document.getElementById('adopt-info');
 const viewMoreButton = document.getElementById('btn-viewMore');
-
 viewMoreButton.addEventListener('click', function(){
     adoptInfo.scrollIntoView({behavior : 'smooth'});
 });
+
+//create 4 categories
+const fetchCatagories = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then((res) => res.json())
+    .then((data) => showCategories(data.categories))
+    .catch((err) => console.log(err))
+};
+
+//show the categories
+const showCategories = (categoriesPet) =>{
+    const categoriesSection = document.getElementById('categories');
+    categoriesPet.forEach((categoriesPetData) =>{
+        console.log(categoriesPetData);
+    const createCategoriesButton = document.createElement('div');
+    createCategoriesButton.innerHTML= `
+    
+    <button class="btn-category btn m-2 w-80 bg-white border border-indigo-200 font-extrabold text-xl">
+    <img class="w-8" src="${categoriesPetData.category_icon}" />
+    ${categoriesPetData.category}
+    </button>`;
+    categoriesSection.append(createCategoriesButton);
+
+    });
+}
+fetchCatagories();
+
+// create card for all pets
